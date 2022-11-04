a0bf52ba69a34e8d00dffa625683aff31d95fa9f
Jordan Harband <ljharb@gmail.com>
2022-11-04 00:09:10 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ToUint32 = require('es-abstract/2021/ToUint32');
+var ToUint32 = require('es-abstract/2022/ToUint32');
 
 module.exports = function imul(x, y) {
 	// taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/imul
